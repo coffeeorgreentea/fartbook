6c98c0ff33334538702927a61d22f7ffd843665f
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2023-03-28 01:38:32 -0400

    ugly image

--- pages/login.tsx
+++ pages/login.tsx
@@ -251,10 +251,10 @@ export default function Login() {
             </div>
           </div>
         </div>
-        <div className="relative flex-1 hidden w-0 opacity-25 lg:block">
+        <div className="relative flex-1 hidden w-0 opacity-10 bg-purple-50 lg:block">
           <img
             className="absolute inset-0 object-cover w-full h-full"
-            src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+            // src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
             alt=""
           />
         </div>
